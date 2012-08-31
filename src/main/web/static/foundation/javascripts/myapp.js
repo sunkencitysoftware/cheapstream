a02d4e9674541b711f2bedbbac4d76d4f29666ee
Andrew Serff <andrew@sunkencity.org>
2012-08-31 16:24:49 -0600

    New version that is mostly working with iTunes and Netflix

--- src/main/web/static/foundation/javascripts/myapp.js
+++ src/main/web/static/foundation/javascripts/myapp.js
@@ -1,48 +1,132 @@
 var searchNetflix = function(movieName) {
     // Build OData query
-//    var movieName = $("#movieName").val();
-console.info('movieName = ' + movieName);
+    //    var movieName = $("#movieName").val();
+    console.info('movieName = ' + movieName);
     var query = "http://odata.netflix.com/Catalog" // netflix base url
     + "/Titles" // top-level resource
-    + "?$filter=substringof('" + escape(movieName) + "',Name)"  // filter by movie name
-    + "&$callback=callback" // jsonp request
+    + "?$filter=Type eq 'Movie' and substringof('" + escape(movieName) + "',Name)"  // filter by movie name
+    + "&$callback=netflixCallback" // jsonp request
     + "&$format=json"; // json request
  
     // Make JSONP call to Netflix
     $.ajax({
         dataType: "jsonp",
         url: query,
-        jsonpCallback: "netflixCallback",
-        success: callback
+        jsonpCallback: "netflixCallback"
     });
 }
 
 var netflixCallback = function(result) {
     // unwrap result
     var movies = result["d"]["results"];
- 
-    // show movies in template
-    var showMovie = tmpl("movieTemplate");
-    var html = "";
-    for (var i = 0; i < movies.length; i++) {
-        // flatten movie
-        movies[i].BoxArtSmallUrl = movies[i].BoxArt.SmallUrl;
- 
-        // render with template
-        html += showMovie(movies[i]);
+    var movie = movies[0];
+    var streamingAvail = movie.Instant.Available;
+    var hdAvail = movie.Instant.HighDefinitionAvailable;
+    var price = 0;
+    if (streamingAvail) {
+        price = "0.00";
+    } else {
+        price = "Unavail";
     }
-    $("#movieTemplateContainer").html(html);
+    var movieForTempl = {
+        image: movie.BoxArt.LargeUrl,
+        url: movie.Url,
+        price: price,
+        hdAvailable: hdAvail,
+        title: movie.ShortName,
+        available: streamingAvail,
+        source: "Netflix"
+    };
+    
+    $( "#movieTemplate" ).tmpl(movieForTempl).appendTo("#movieList");
+    
 }
         
+var searchITunes = function(movieName) {
+    var query = "http://itunes.apple.com/search";
+    var params = {
+        entity: "movie", 
+        term: movieName
+    };
+    $.ajax({
+        url: query,
+        dataType: 'jsonp',
+        jsonpCallback: "itunesCallback",
+        data: params
+    });
+}
+
+var itunesCallback = function(result) {
+    if (result && result.results) {
+        var movies = result.results;
+    
+        console.info(movies.length + " movies from iTunes");
+        if (movies && movies.length > 0) {
+            var movie = movies[0];
+            var movieForTempl = {
+                image: movie.artworkUrl100,
+                url: movie.trackViewUrl,
+                price: movie.trackPrice,
+                title: movie.trackName,
+                hdAvailable: "Unknown",
+                available: true,
+                source: "iTunes"
+            };
+            $( "#movieTemplate" ).tmpl(movieForTempl).appendTo("#movieList");
+        }
+    }
+}
+var searchAmazon = function(movieName) {
+    var query = "http://itunes.apple.com/search";
+    var params = {
+        entity: "movie", 
+        term: movieName
+    };
+    $.ajax({
+        url: query,
+        dataType: 'jsonp',
+        jsonpCallback: "itunesCallback",
+        data: params
+    });
+}
+
+var amazonCallback = function(result) {
+    if (result && result.results) {
+        var movies = result.results;
+    
+        console.info(movies.length + " movies from iTunes");
+        if (movies && movies.length > 0) {
+            var movie = movies[0];
+            var movieForTempl = {
+                image: movie.artworkUrl100,
+                url: movie.trackViewUrl,
+                price: movie.trackPrice,
+                title: movie.trackName,
+                hdAvailable: "Unknown",
+                available: true,
+                source: "iTunes"
+            };
+            $( "#movieTemplate" ).tmpl(movieForTempl).appendTo("#movieList");
+        }
+    }
+}
+
+var handleSearch = function(searchTerm) {
+    $("#movieList").children().remove();
+    searchNetflix(searchTerm);
+    searchITunes(searchTerm);
+}
         
 $(function() {
     // Handler for .ready() called.
     $('#search').submit(function() {
-        searchNetflix($('#searchInput').val());
+        var searchTerm = $('#searchInput').val();
+        handleSearch(searchTerm);
         return false;
     });
     $('#searchButton').click(function() {
-        searchNetflix($('#searchInput').val());
+        var searchTerm = $('#searchInput').val();
+        handleSearch(searchTerm);
         return false;
     });
 });
